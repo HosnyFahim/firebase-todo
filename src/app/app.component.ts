eb1d947a1056173eb160e9c2c87ca18ce3e008cc
HosnyFahim <hosny.fahim1990@gmail.com>
2023-06-21 22:08:18 +0200

    toDo done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { inject } from '@angular/core';
+import { Firestore, collectionData, collection, setDoc, doc } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+
+  
 })
 export class AppComponent {
-  title = 'firebase-todolist';
+    item!: Observable<any>;
+    firestore: Firestore = inject(Firestore);
+    newTodos: Array<any>;
+    todoText:string = '';
+
+    constructor() {
+      const coll = collection(this.firestore, 'todos');
+      this.item = collectionData(coll);
+
+      this.item.subscribe((newItem) => {
+        console.log('neue Todos sind:', newItem);
+        this.newTodos = newItem;
+      });
+    }
+
+    addTodo() {
+      const coll = collection(this.firestore, 'todos');
+      setDoc(doc(coll), {name: this.todoText});
+    }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'todolist-9cfc7',
+    appId: '1:248458090390:web:2ee49342cabf99c0592fb7',
+    storageBucket: 'todolist-9cfc7.appspot.com',
+    apiKey: 'AIzaSyCNMaf4GAQHme_82EUHOk_ScKzrfppMBEA',
+    authDomain: 'todolist-9cfc7.firebaseapp.com',
+    messagingSenderId: '248458090390',
+    measurementId: 'G-WF3GFED285',
+  },
+};
